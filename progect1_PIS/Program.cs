8053d48be15b79d18a7fab41142a7911ca150a3c
polin <polin@POLINA>
2024-09-17 21:07:07 +0700

    Добавьте файлы проекта.

---
+++ progect1_PIS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progect1_PIS
+{
+    class Program
+    {
+        static void Main()
+        
+        {
+            ThemesOfTheWorks themesOfTheWorks =
+            new ThemesOfTheWorks("\"Казарез Полина Андреевна\" \"Курсовая работа\" 2024.10.20");
+
+            Console.WriteLine(themesOfTheWorks.ToString());
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ progect1_PIS/ThemesOfTheWorks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace progect1_PIS
+{
+    class ThemesOfTheWorks
+    {
+        public string StudentName { get; }
+        public string Theme { get; }
+        public DateTime DateOfIssue { get; }
+
+        public ThemesOfTheWorks(string input)
+        {
+            var parts = Regex.Matches(input, @"[\""].+?[\""]|[^ ]+").Cast<Match>().Select(m => m.Value).ToList();
+            StudentName = parts[0].Trim('/', '"');
+            Theme = parts[1].Trim('/', '"');
+            DateOfIssue = DateTime.ParseExact(parts[2], "yyyy.MM.dd", CultureInfo.InvariantCulture);
+        }
+        public override string ToString()
+        {
+            return $"Имя студента: {StudentName}; \fТема работы: {Theme}; \fДата выдачи: {DateOfIssue}";
+            
+        }
+
+    }
+}
