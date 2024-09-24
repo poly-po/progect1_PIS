ce920439c344e3be9a419e654b9e2583d319b394
poly-po <polina.kazarezzz@gmail.com>
2024-09-25 01:27:44 +0700

    вторая лаба

--- progect1_PIS/Program.cs
+++ progect1_PIS/Program.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace progect1_PIS
 {
     class Program
     {
-        static void Main()
-        
+        static void Main(string[] args)
         {
-            ThemesOfTheWorks themesOfTheWorks =
-            new ThemesOfTheWorks("\"Казарез Полина Андреевна\" \"Курсовая работа\" 2024.10.20");
+            Console.OutputEncoding = Encoding.UTF8;
+            MainString mainstr = new MainString();
 
-            Console.WriteLine(themesOfTheWorks.ToString());
+            string income1 = "\"Статус работы\" \"Казарез Полина Андреевна\" \"Курсовая работа\" 2023.10.02 \"В процессе\"";
+            string income2 = "\"Тема работы\" \"Спепанова Лидия Ивановна\" \"Практическая работа\" 2024.09.09";
+            string income3 = "\"Работа с наставником\" \"Казарез Полина Андреевна\" \"Курсовая работа\" 2023.10.02 \"Иванов Михаил Ильич\"";
+           
+
+            Console.WriteLine("Вывод из строк");
+            string[] provera = new string[] { income1, income2, income3};
+            foreach (string linestr in provera)
+            {
+                Console.WriteLine(mainstr.ObjectOutput(linestr));
+            }
+
+            Console.WriteLine("\nВывод из файла");
+            foreach (string linefile in mainstr.StrFromFiles("2.txt"))
+            {
+                Console.WriteLine(mainstr.ObjectOutput(linefile));
+            }
             Console.ReadKey();
         }
     }
--- progect1_PIS/ThemesOfTheWorks.cs
+++ progect1_PIS/ThemesOfTheWorks.cs
@@ -1,31 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
 
 namespace progect1_PIS
 {
     class ThemesOfTheWorks
     {
-        public string StudentName { get; }
-        public string Theme { get; }
-        public DateTime DateOfIssue { get; }
+        public string Type { get; set; }
+        public string StudentsName { get; set; }
+        public string TopicName { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        
+
 
         public ThemesOfTheWorks(string input)
         {
-            var parts = Regex.Matches(input, @"[\""].+?[\""]|[^ ]+").Cast<Match>().Select(m => m.Value).ToList();
-            StudentName = parts[0].Trim('/', '"');
-            Theme = parts[1].Trim('/', '"');
-            DateOfIssue = DateTime.ParseExact(parts[2], "yyyy.MM.dd", CultureInfo.InvariantCulture);
-        }
-        public override string ToString()
-        {
-            return $"Имя студента: {StudentName}; \fТема работы: {Theme}; \fДата выдачи: {DateOfIssue}";
-            
+            var parts = MainString.ParseString(input);
+            Type = parts[0].Trim('"');
+            StudentsName = parts[1].Trim('"');
+            TopicName = parts[2].Trim('"');
+            DateOfIssue = DateTime.ParseExact(parts[3], "yyyy.MM.dd", null);
         }
 
+        public virtual string LineOutput()
+        {
+            return $"{Type}: Имя студента: {StudentsName}, Название темы: {TopicName}, Дата выдачи: {DateOfIssue.ToString("yyyy.MM.dd")} ";
+        }
+    }
+    class WorksWithAMentor : ThemesOfTheWorks
+    {
+        public string MentorsName { get; set; }
+
+        public WorksWithAMentor(string input) : base(input)
+        {
+            var parts = MainString.ParseString(input);
+            MentorsName = parts[4].Trim('"');
+        }
+
+        public override string LineOutput()
+        {
+            return $"{Type}: Имя студента: {StudentsName}, Название темы: {TopicName}, Дата выдачи: {DateOfIssue.ToString("yyyy.MM.dd")}, Имя наставника: {MentorsName} ";
+        }
+    }
+    class StatusOfWorks : ThemesOfTheWorks
+    {
+        public string Status { get; set; }
+
+        public StatusOfWorks(string input) : base(input)
+        {
+            var parts = MainString.ParseString(input);
+            Status = parts[4].Trim('"');
+        }
+        public override string LineOutput()
+        {
+            return $"{Type}: Имя студента: {StudentsName}, Название темы: {TopicName}, Дата выдачи: {DateOfIssue.ToString("yyyy.MM.dd")}, Статус работы: {Status}";
+        }
+    }
+
+    class MainString
+    {
+        public string[] StrFromFiles(string filePath)
+        {
+            string fileContent = File.ReadAllText(filePath);
+            return fileContent.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static List<string> ParseString(string input)
+        {
+            return Regex.Matches(input, @"[\""].+?[\""]|[^ ]+")
+                         .Cast<Match>()
+                         .Select(m => m.Value)
+                         .ToList();
+        }
+
+        public string ObjectOutput(string stroka)
+        {
+            var parts = ParseString(stroka);
+
+            if (parts[0].Trim('"') == "Тема работы")
+            {
+                ThemesOfTheWorks type1 = new ThemesOfTheWorks(stroka);
+                return type1.LineOutput();
+            }
+            else if (parts[0].Trim('"') == "Работа с наставником")
+            {
+                WorksWithAMentor type2 = new WorksWithAMentor(stroka);
+                return type2.LineOutput();
+            }
+            else if (parts[0].Trim('"') == "Статус работы")
+            {
+               StatusOfWorks type3 = new StatusOfWorks(stroka);
+                return type3.LineOutput();
+            }
+            else
+                return "Возникла ошибка";
+        }
     }
 }
